11c8d5f2f34ae58fcf5ce655de294cbd9d5bd2d0
Javier Perez <javier_hp05@hotmail.com>
2025-03-04 21:50:59 -0300

    Inicio proyecto Ecommerce con React/ le agrege un navbar sin funcionalidades

---
+++ src/components/Header/Navbar.jsx
@@ -0,0 +1,20 @@
+import './Navbar.css'
+
+function Navbar() {
+
+  return (
+    <>
+      <header>
+        <ul className='nav-bar'>
+          <li className='item-nav-bar'>LOGO</li>
+          <li className='item-nav-bar'>Inicio</li>
+          <li className='item-nav-bar'>Productos</li>
+          <li className='item-nav-bar'>Contacto</li>
+          <li className='item-nav-bar'>Carrito</li>
+        </ul>
+      </header>
+    </>
+  )
+}
+
+export default Navbar
